dbc0e2fd81c2d3ba98fb3ebe378e4f2f80902949
Henrique Montibeller <iquemontibeller@gmail.com>
2019-08-21 08:24:19 -0300

    Feita a Classe Veiculo

---
+++ Model/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("veiculos")]
+    public class Veiculo
+    {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("placa")]
+        public string Placa { get; set; }
+
+        [Column("id_modelo")]
+        public int IdModelo { get; set; }
+
+        [ForeignKey("IdModelo")]
+        public Modelo Modelo { get; set; }
+    }
+}
