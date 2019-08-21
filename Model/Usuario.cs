3520d39bd2ecb9301d57c440a22d2e19ace34496
Henrique Montibeller <iquemontibeller@gmail.com>
2019-08-21 08:38:41 -0300

    Feita a Classe do Usuario

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("usuarios")]
     public class Usuario
     {
+        [Key, Column("id")]
+        public int Id { get; set; }
 
+        [Column("login")]
+        public string Login { get; set; }
+
+        [Column("senha")]
+        public string Senha { get; set; }
+
+        [Column("admin")]
+        public bool Admin { get; set; }
     }
 }
