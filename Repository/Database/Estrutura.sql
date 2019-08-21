fec96ce50dba0b6b5cb9cd463000185f44c41ee7
Henrique Montibeller <iquemontibeller@gmail.com>
2019-08-21 10:42:38 -0300

    Arrumado o Banco de Dados pt2

--- Repository/Database/Estrutura.sql
+++ Repository/Database/Estrutura.sql
@@ -1,21 +1,21 @@
 ﻿DROP TABLE IF EXISTS notas_fiscais, rotas, vendas, produtos, vendedores, veiculos, modelos, clientes, categorias, marcas, usuarios;
 CREATE TABLE usuarios( 
-id_usuario INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
 senha VARCHAR(45)
 );
 
 CREATE TABLE marcas (
-id_marca INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45)
 );
 
 CREATE TABLE categorias( 
-id_categoria INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45));
 
 CREATE TABLE clientes(
-id_cliente INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome  VARCHAR(45),
 telefone VARCHAR(20),
 cnpj VARCHAR (18),
@@ -23,60 +23,60 @@ cep  VARCHAR(9)
 );
 
 CREATE TABLE modelos(
-id_modelo INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
 id_marca INT,
-FOREIGN KEY (id_marca) REFERENCES marcas(id_marca)
+FOREIGN KEY (id_marca) REFERENCES marcas(id)
 );
 
 CREATE TABLE veiculos(
-id_veiculo INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 placa VARCHAR(10),
 descrição VARCHAR(100),
 id_modelo iNT,
-FOREIGN KEY (id_modelo) REFERENCES modelos(id_modelo)
+FOREIGN KEY (id_modelo) REFERENCES modelos(id)
 );
 
 CREATE TABLE vendedores(
-id_vendedor INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 id_usuario INT,
 id_veiculo INT,
-FOREIGN KEY(id_veiculo) REFERENCES veiculos(id_veiculo),
-FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario));
+FOREIGN KEY(id_veiculo) REFERENCES veiculos(id),
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id));
 
 CREATE TABLE produtos(
-id_produto INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 id_categoria INT,
 codigo_barra VARCHAR(12),
 quantidade_produto INT,
 valor_unitario DECIMAL(8,2),
-FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
+FOREIGN KEY (id_categoria) REFERENCES categorias(id)
 );
 
 CREATE TABLE vendas(
-id_venda INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 quantidade INT,
 id_vendedor INT,
 id_cliente INT,
 id_produto INT,
-FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor),
-FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+FOREIGN KEY (id_vendedor) REFERENCES vendedores(id),
+FOREIGN KEY (id_cliente) REFERENCES clientes(id),
+FOREIGN KEY (id_produto) REFERENCES produtos(id)
 );
 
 CREATE TABLE rotas(
-id_rota INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
 istancia DECIMAL(5,2),
 id_vendedor INT,
-FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
+FOREIGN KEY (id_vendedor) REFERENCES vendedores(id)
 );
 
 CREATE TABLE notas_fiscais(
-id_notafiscal INT PRIMARY KEY IDENTITY(1,1),
+id INT PRIMARY KEY IDENTITY(1,1),
 valorfinal DECIMAL(7,2),
 id_vendas INT,
-FOREIGN KEY (id_vendas) REFERENCES vendas(id_venda)
+FOREIGN KEY (id_vendas) REFERENCES vendas(id)
 );
 
 
