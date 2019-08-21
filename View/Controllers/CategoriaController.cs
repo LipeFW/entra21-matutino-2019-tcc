4282c12b33640aa0ad7060db92757197c624d2df
Henrique Montibeller <iquemontibeller@gmail.com>
2019-08-21 09:34:26 -0300

    Feita a rotina Obter Todos do Categoria controller

---
+++ View/Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    [Route("categoria/")]
+    public class CategoriaController : Controller
+    {
+        CategoriaRepository repository;
+
+        public CategoriaController()
+        {
+            repository = new CategoriaRepository();
+        }
+
+        [HttpGet, Route("obtertodos")]
+        public JsonResult ObterTodos()
+        {
+            var categorias = repository.ObterTodos();
+            var resultado = new { data = categorias };
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+            
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
