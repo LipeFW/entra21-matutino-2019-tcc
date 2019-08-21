851036ab2cecb20de2bcd0ff99bb4068bc23ec38
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-08-21 08:05:22 -0300

    Criado os Model Produto, Categoria e Clientes

---
+++ Model/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("clientes")]
+    public class Cliente
+    {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("telefone")]
+        public string Telefone { get; set; }
+
+        [Column("cnpj")]
+        public string CNPJ { get; set; }
+
+        [Column("cpf")]
+        public string CPF { get; set; }
+
+        [Column("id_vendedor")]
+        public int IdVendedor { get; set; }
+
+        [ForeignKey("IdVendedor")]
+        public Vendedor Vendedor { get; set; }
+    }
+}
