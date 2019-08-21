07d34b16cfcfa1622f1382c63b24b62f70b01b73
Felipe F. Weber <felipeweber2002@hotmail.com>
2019-08-21 10:13:45 -0300

    Criado LayoutHtml; UsuarioController; Index de Usuarios; Criada Modal Cadastro de Usuarios; Corrigido Nome UsuarioRepository;

---
+++ View/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private UsuarioRepository repository;
+
+        public UsuarioController()
+        {
+            repository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
