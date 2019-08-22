202ddb90b7b0168ebfa9dc7fa4ceaeb825f6bda6
Felipe F. Weber <felipeweber2002@hotmail.com>
2019-08-22 07:59:42 -0300

    Corrigido mesclagem

--- Repository/Interface/ICategoriaRepository.cs
+++ Repository/Interface/ICategoriaRepository.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Interface
 {
-<<<<<<< HEAD
     public interface ICategoriaRepository
-=======
-    public class ICategoriaRepository
->>>>>>> 95eae6f8ca8e806746d641e3e0f898a42ce42606
     {
         int inserir();
         bool alterar();
--- Repository/Interface/IClienteRepository.cs
+++ Repository/Interface/IClienteRepository.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Interface
 {
-<<<<<<< HEAD
     public interface IClienteRepository
-=======
-    public class IClienteRepository
->>>>>>> 95eae6f8ca8e806746d641e3e0f898a42ce42606
     {
         int inserir();
         bool alterar();
