46de7dce534d91b7c34553309eac36942a78cdf3
HenriqueLinharesMontibeller <iquemontibeller@gmail.com>
2019-08-22 09:47:33 -0300

    Feito mais alguns ajustes

--- View/Controllers/ProdutoController.cs
+++ View/Controllers/ProdutoController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 
 namespace View.Controllers
 {
-    [Route("produto")]
+    [Route("produto/")]
     public class ProdutoController : Controller
     {
         ProdutoRepository repository;
