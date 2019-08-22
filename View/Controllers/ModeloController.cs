22152c62c64b2caf78327690befe1669fa0ff60a
Eduardo Hausmann <eduardohausmann1705@gmail.com>
2019-08-22 07:57:26 -0300

    Feito o Controller do Modelo

--- View/Controllers/ModeloController.cs
+++ View/Controllers/ModeloController.cs
@@ -1,4 +1,5 @@
-﻿using Repository.Repositories;
+﻿using Model;
+using Repository.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 
 namespace View.Controllers
 {
+    [Route("modelo/")]
     public class ModeloController : Controller
     {
         private ModeloRepository repository;
@@ -16,8 +18,56 @@ namespace View.Controllers
             repository = new ModeloRepository();
         }
 
+        [HttpGet, Route("obtertodos")]
+        public JsonResult ObterTodos()
+        {
+            var modelos = repository.ObterTodos();
+            var resultado = new { data = modelos };
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost, Route("cadastro")]
+        public ActionResult Cadastro(Modelo modelo)
+        {
+            int id = repository.Inserir(modelo);
+            return RedirectToAction("Editar", new { id = id });
+        }
+
+        [HttpPost, Route("editar")]
+        public JsonResult Editar(Modelo modelo)
+        {
+            var alterou = repository.Alterar(modelo);
+            var resultado = new { status = alterou };
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet, Route("apagar")]
+        public JsonResult Apagar(int id)
+        {
+            var apagou = repository.Apagar(id);
+            var resultado = new { status = apagou };
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            var modelo = repository.Alterar(id);
+            if (modelo == null)
+                return RedirectToAction("Index");
+
+            ViewBag.Modelo = modelo;
             return View();
         }
     }
