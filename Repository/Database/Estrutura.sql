3ee1d502ac30f08ecb33afe4244607d7f0da32a9
Eduardo Hausmann <eduardohausmann1705@gmail.com>
2019-08-23 10:54:48 -0300

    Arrumada a Estrutura do Banco de Dados

--- Repository/Database/Estrutura.sql
+++ Repository/Database/Estrutura.sql
@@ -3,7 +3,8 @@
 CREATE TABLE usuarios( 
 id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
-senha VARCHAR(45)
+senha VARCHAR(45),
+registro_ativo BIT
 );
 
 INSERT INTO usuarios (nome,senha)
@@ -11,7 +12,8 @@ VALUES ('Josefino','jsf1520');
 
 CREATE TABLE marcas (
 id INT PRIMARY KEY IDENTITY(1,1),
-nome VARCHAR(45)
+nome VARCHAR(45),
+registro_ativo BIT
 );
 
 INSERT INTO marcas(nome)
@@ -19,7 +21,8 @@ VALUES ('Ford');
 
 CREATE TABLE categorias( 
 id INT PRIMARY KEY IDENTITY(1,1),
-nome VARCHAR(45));
+nome VARCHAR(45),
+registro_ativo BIT);
 
 INSERT INTO categorias(nome)
 VALUES ('Bebidas');
@@ -29,7 +32,8 @@ id INT PRIMARY KEY IDENTITY(1,1),
 nome  VARCHAR(45),
 telefone VARCHAR(20),
 cnpj VARCHAR (18),
-cep  VARCHAR(9)
+cep  VARCHAR(9),
+registro_ativo BIT
 );
 
 INSERT INTO clientes(nome, telefone, cnpj, cep)
@@ -39,7 +43,8 @@ CREATE TABLE modelos(
 id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
 id_marca INT,
-FOREIGN KEY (id_marca) REFERENCES marcas(id)
+FOREIGN KEY (id_marca) REFERENCES marcas(id),
+registro_ativo BIT
 );
 
 INSERT INTO modelos(nome,id_marca)
@@ -49,7 +54,8 @@ CREATE TABLE veiculos(
 id INT PRIMARY KEY IDENTITY(1,1),
 placa VARCHAR(10),
 id_modelo iNT,
-FOREIGN KEY (id_modelo) REFERENCES modelos(id)
+FOREIGN KEY (id_modelo) REFERENCES modelos(id),
+registro_ativo BIT
 );
 
 INSERT INTO veiculos(placa,id_modelo)
@@ -60,7 +66,8 @@ id INT PRIMARY KEY IDENTITY(1,1),
 id_usuario INT,
 id_veiculo INT,
 FOREIGN KEY(id_veiculo) REFERENCES veiculos(id),
-FOREIGN KEY (id_usuario) REFERENCES usuarios(id));
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+registro_ativo BIT);
 
 INSERT INTO vendedores (id_usuario, id_veiculo)
 VALUES (1,1);
@@ -71,7 +78,8 @@ id_categoria INT,
 codigo_barra VARCHAR(12),
 quantidade_produto INT,
 valor_unitario DECIMAL(8,2),
-FOREIGN KEY (id_categoria) REFERENCES categorias(id));
+FOREIGN KEY (id_categoria) REFERENCES categorias(id),
+registro_ativo BIT);
 
 INSERT INTO produtos (id_categoria, codigo_barra, quantidade_produto, valor_unitario)
 VALUES (1, 'xxxxxxxxxxxx', 1, 1);
@@ -84,7 +92,8 @@ id_cliente INT,
 id_produto INT,
 FOREIGN KEY (id_vendedor) REFERENCES vendedores(id),
 FOREIGN KEY (id_cliente) REFERENCES clientes(id),
-FOREIGN KEY (id_produto) REFERENCES produtos(id));
+FOREIGN KEY (id_produto) REFERENCES produtos(id),
+registro_ativo BIT);
 
 INSERT INTO vendas (quantidade,id_vendedor,id_cliente,id_produto)
 VALUES (1,1,1,1);
@@ -93,7 +102,8 @@ CREATE TABLE rotas(
 id INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(45),
 id_vendedor INT,
-FOREIGN KEY (id_vendedor) REFERENCES vendedores(id)
+FOREIGN KEY (id_vendedor) REFERENCES vendedores(id),
+registro_ativo BIT
 );
 
 INSERT INTO rotas(nome,id_vendedor)
@@ -103,7 +113,8 @@ CREATE TABLE notas_fiscais(
 id INT PRIMARY KEY IDENTITY(1,1),
 valor_final DECIMAL(7,2),
 id_vendas INT,
-FOREIGN KEY (id_vendas) REFERENCES vendas(id)
+FOREIGN KEY (id_vendas) REFERENCES vendas(id),
+registro_ativo BIT
 );
 
 INSERT INTO notas_fiscais(valor_final,id_vendas)
