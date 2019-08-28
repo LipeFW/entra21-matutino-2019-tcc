d7066525d2415c5145c65f8fb51426825fa52c44
Nathan t <nathantheiss@gmail.com>
2019-08-28 11:24:14 -0300

    Simplificando os Repository

---
+++ Repository/Repositories/VeiculoRepository.cs
@@ -0,0 +1,37 @@
+﻿using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class VeiculoRepository : IVeiculoRepository
+    {
+        public bool Alterar(Veiculo veiculo)
+        {
+            
+        }
+
+        public bool Apagar(int id)
+        {
+           
+        }
+
+        public int Inserir(Veiculo veiculo)
+        {
+            
+        }
+
+        public Veiculo ObterPeloId(int id)
+        {
+           
+        }
+
+        public List<Veiculo> ObterTodos()
+        {
+           
+        }
+    }
+}
