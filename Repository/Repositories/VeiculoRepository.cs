dbe367c8e862a594fe9252ded808b5bb4f25e9e4
Nathan t <nathantheiss@gmail.com>
2019-08-29 08:36:52 -0300

    Finalizado O veiculo Repository

--- Repository/Repositories/VeiculoRepository.cs
+++ Repository/Repositories/VeiculoRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model;
 namespace Repository.Repositories
 {
     public class VeiculoRepository : IVeiculoRepository
@@ -20,31 +20,60 @@ namespace Repository.Repositories
 
         public bool Apagar(int id)
         {
-            var veiculo
+            var veiculo = context.Veiculos.FirstOrDefault(x => x.Id == id);
+            if (veiculo == null)
+                return false;
+
+            veiculo.RegistroAtivo = false;
+            int quantidadeAfetada = context.SaveChanges();
+            return quantidadeAfetada == 1;
 
         }
 
-
-        public bool Alterar(Veiculo veiculo)
-        {
-             
-        }
-
-     
 
         public int Inserir(Veiculo veiculo)
         {
-            
+           veiculo.RegistroAtivo = true;
+            context.Veiculos.Add(veiculo);
+            context.SaveChanges();
+            return veiculo.Id;
+
         }
 
         public Veiculo ObterPeloId(int id)
         {
-           
+            return context.Veiculos.FirstOrDefault(x => x.Id == id);
+
         }
+
 
         public List<Veiculo> ObterTodos()
         {
-           
+            return context.Veiculos.Where(x => x.RegistroAtivo == true).ToList();
+
         }
+        public bool Alterar(Veiculo veiculo)
+        {
+            var veiculooriginal = context.Veiculos.FirstOrDefault(x => x.Id == veiculo.Id);
+
+            if (veiculooriginal == null)
+                return false;
+
+            veiculooriginal.Id = veiculo.Id;
+            veiculooriginal.Marca = veiculo.Marca;
+            veiculooriginal.Modelo = veiculo.Modelo;
+            veiculooriginal.NumeroCaminhao = veiculo.NumeroCaminhao;
+            veiculooriginal.Placa = veiculo.Placa;
+            veiculooriginal.RegistroAtivo = veiculo.RegistroAtivo;
+
+            int quantidadeafetada = context.SaveChanges();
+            return quantidadeafetada == 1;
+        }
+
+     
+
+
+
+     
     }
 }
