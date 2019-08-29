9a1e4e80a44c581545a748531393897d1164c210
Henrique Montibeller <iquemontibeller@gmail.com>
2019-08-29 09:52:25 -0300

    Arrumado o modal do produto,

---
+++ View/Scripts/Produto/produto.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $idAlterar = -1;
+
+    $tabelaProduto = $('#produtos-index').DataTable({
+        ajax: 'http://localhost:51242/Produto/obtertodos',
+        serverSide: true,
+        columns: [
+            { 'data': 'Id' },
+
+        ]
+    })
+})
