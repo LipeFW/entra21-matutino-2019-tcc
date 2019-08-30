5b3f5df40ca5014c1da1ac67173f46a2853f5245
Pablo Luz <pabloluz46@gmail.com>
2019-08-30 10:28:43 -0300

    Adicionado o tema ao projeto

--- View/Scripts/Vendedor/Vendedor.js
+++ View/Scripts/Vendedor/Vendedor.js
@@ -1,1 +1,104 @@
-﻿
+﻿$(function () {
+    $idAlterar = -1;
+
+    $tabelaVeiculo = $('#vendedor-index').DataTable({
+        ajax: 'http://localhost:51242/vendedor/obtertodos',
+        serverSide: true,
+        columns: [
+            { 'data': 'Id' },
+            { 'data': 'usuario' },
+            { 'data': 'Veiculo' },
+            {
+                render: function (data, type, row) {
+                    return "<button class='btn btn-primary botao-editar' data-id='" + row.Id + "'>Editar</button>\
+                    <button class='btn btn-danger botao-apagar ml-2' data-id='"+ row.Id + "'>Apagar</button>";
+                }
+            }
+        ]
+    });
+
+    $('#vendedor-botao-salvar').on('click', function () {
+        $marca = $('#vendedor-campo-usuario').val();
+        $mdodelo = $('#vendedor-campo-veiculo').val();
+
+
+        if ($idAlterar == -1) {
+            inserir($usuario, $veiculo);
+        } else {
+            alterar($usuario, $veiculo);
+        }
+    });
+
+    function alterar($usuario, $veiculo) {
+        $.ajax({
+            url: 'http://localhost:51242/vendedor/update',
+            method: 'post',
+            data: {
+                id: $isAlterar,
+                usuario: $usuario,
+                veiculo: $veiculo,
+
+            },
+            success: function (data) {
+                $('modal-vendedor').modal('hide');
+                $idAlterar = -1;
+                $tabelaVeiculo.ajax.reload();
+            },
+            error: function (err) {
+                alert('Não foi possivel alterar');
+            }
+        });
+    }
+
+    function inserir($usuario, $veiculo) {
+        $.ajax({
+            url: 'http://localhost:51242/vendedor/inserir',
+            method: 'post',
+            data: {
+                id: $isInserir,
+                usuario: $usuario,
+                veiculo: $veiculo,
+
+            },
+            success: function (data) {
+                $('modal-vendedor').modal('hide');
+                $tabelaVeiculo.ajax.reload();
+            },
+            error: function (err) {
+                alert('Não foi possivel inserir');
+            }
+        });
+    }
+
+    $('.table').on('click', '.botao-apagar', function () {
+        $idApagar = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost:51242/vendedor/apagar?id=' + $idApagar,
+            method: 'get',
+            success: function (data) {
+                $tabelaVeiculo.ajax.reload();
+            },
+            error: function (err) {
+                alert('Não foi possivel apagar');
+            }
+        });
+    });
+
+    $('.table').on('click', '.botao-editar', function () {
+        $idAlterar = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost:51242/vendedor/editar?id=' + $idAlterar,,
+            method: 'get',
+            success: function (data) {
+                $('#vendedor-campo-usuario').val(data.Usuario);
+                $('#vendedor-campo-veiculo').val(data.Veiculo);
+
+            },
+            error: function (err) {
+                alert('Não foi possivel carregar');
+            }
+        });
+    });
+});
