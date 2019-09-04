0e84a8da213eb112b0d2113bf006a25725e43f63
Felipe Franco Weber <felipeweber2002@hotmail.com>
2019-09-03 23:51:09 -0300

    Corrigido alguns detalhes de estética, continuação na tradução das páginas

--- View/Content/js/lang.js
+++ View/Content/js/lang.js
@@ -5,6 +5,14 @@
         $("#langAtual").text("Português");
         $("#enLang").text("Inglês");
         $("#ptLang").text("Português");
+        $("#inicioText").text("Início");
+        $("#usuarioText").text("Usuário");
+        $("#categoriaText").text("Categoria");
+        $("#veiculoText").text("Veículo");
+        $("#produtoText").text("Produto");
+        $("#bemvindoText").text("Bem Vindo");
+        $("#sairText").text("Sair");
+        $("#configText").text("Configurações da Conta");
         $("#imgLANG").attr("src", "Content/theme/global_assets/images/lang/br.png");
     });
     $("#btnEN").on("click", function () {
@@ -13,6 +21,14 @@
         $("#langAtual").text("English");
         $("#enLang").text("English");
         $("#ptLang").text("Portuguese");
+        $("#inicioText").text("Home");
+        $("#usuarioText").text("User");
+        $("#categoriaText").text("Category");
+        $("#veiculoText").text("Vehicle");
+        $("#produtoText").text("Product");
+        $("#bemvindoText").text("Welcome");
+        $("#sairText").text("Logout");
+        $("#configText").text("Account Config");
         $("#imgLANG").attr("src","Content/theme/global_assets/images/lang/gb.png");
     });
 });
