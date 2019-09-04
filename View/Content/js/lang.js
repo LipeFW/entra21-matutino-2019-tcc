92ca77c756e88e90bde3a13aa0b1b5f0b2090d66
Felipe Franco Weber <felipeweber2002@hotmail.com>
2019-09-03 22:53:06 -0300

    Corrigido problemas na seleção de linguagem

---
+++ View/Content/js/lang.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#btnPT").on("click", function () {
+        $("#btnPT").addClass("active");
+        $("#btnEN").removeClass("active");
+        $("#langAtual").text("Português");
+        $("#enLang").text("Inglês");
+        $("#ptLang").text("Português");
+        $("#imgLANG").attr("src", "Content/theme/global_assets/images/lang/br.png");
+    });
+    $("#btnEN").on("click", function () {
+        $("#btnEN").addClass("active");
+        $("#btnPT").removeClass("active");
+        $("#langAtual").text("English");
+        $("#enLang").text("English");
+        $("#ptLang").text("Portuguese");
+        $("#imgLANG").attr("src","Content/theme/global_assets/images/lang/gb.png");
+    });
+});
