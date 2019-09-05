a6033ad1f1ac4e6047dd304a9f36b2cded1572aa
Felipe F. Weber <felipeweber2002@hotmail.com>
2019-09-05 10:20:35 -0300

    Iniciado script de login

--- View/Content/js/login.js
+++ View/Content/js/login.js
@@ -1,2 +1,16 @@
-﻿$(function tentarLogin () {
-});
+﻿function Login() {
+    var done = 0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementsByName('senha')[0].value;
+    seha = senha.toLowerCase();
+    if (usuario == "admin" && senha == "admin") {
+        done = 1;
+    }
+    if (done == 0) { alert("Usuário ou senha inválidos"); }
+
+    if (done == 1) {
+        window.location.href = "/home/index";
+
+    }
+}
