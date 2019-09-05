668b528c4192dfca2daf3fef51c3cee84ef08f64
Pablo Luz <pabloluz46@gmail.com>
2019-09-05 09:15:53 -0300

    Reformulado produto.js

--- View/Scripts/Produto/produto.js
+++ View/Scripts/Produto/produto.js
@@ -3,14 +3,14 @@
     $idAlterar = -1;
 
     $tabelaProduto = $('#produto-tabela').DataTable({
-        ajax: 'http://localhost:51242/Produto/obtertodos',
+        ajax: "http://localhost:51242/Produto/obtertodos",
         serverSide: true,
         columns: [
-            { 'data': 'Id' },
-            { 'data': 'Categoria' },
-            { 'data': 'Código Barra' },
-            { 'data': 'Quantidade Produtos' },
-            { 'data': 'Valor Unitario'},
+            { "data": "Id" },
+            { "data": "Categoria" },
+            { "data": "Código Barra" },
+            { "data": "Quantidade Produtos" },
+            { "data": "Valor Unitario"},
             {
                 render: function (data, type, row) {
                     return '<button class="btn btn-primary mb-3 botao-editar"data-id="' + row.Id +'">Editar</button>/<button class="btn btn-danger mb-3 botao-apagar" data-id="' + row.Id + '"Apagar</button>'
@@ -19,11 +19,11 @@
         ]
     });
 
-    $('#produto-botao-salvar').on('#click', function () {
-        $categoria = $('#produto-campo-Categoria').val();
-        $codigoBarra = $('#produto-campo-codigo-barra').val();
-        $quantidadeProdutos = $('#produto-campo-quantidade-produtos').val();
-        $valorUnitario = $('#produto-campo-valor-unitario').val();
+    $("#produto-botao-salvar").on("#click", function () {
+        $categoria = $("#produto-campo-Categoria").val();
+        $codigoBarra = $("#produto-campo-codigo-barra").val();
+        $quantidadeProdutos = $("#produto-campo-quantidade-produtos").val();
+        $valorUnitario = $("#produto-campo-valor-unitario").val();
 
         if ($idAlterar == -1) {
             inserir($categoria, $cofigoBarra, $quantidadeProdutos, $valorUnitario);
@@ -33,7 +33,7 @@
 
         function alterar($categoria, $codigoBarra, $quantidadeProdutos, $valorUnitario) {
             $.ajax({
-                url: 'http://localhost:51242/Produto/update',
+                url: "http://localhost:51242/Produto/update",
                 method: "post",
                 data: {
                     id: $idAlterar,
@@ -43,19 +43,19 @@
                     valorUnitario: $valorUnitario
                 },
                 success: function (data) {
-                    $('#modal-produto').modal('hide');
+                    $("#modal-produto").modal("hide");
                     $idAlterar = -1;
                     $tabelaProduto.ajax.reload();
                 },
                 error: function (err) {
-                    alert('Não foi possivel alterar o Produto');
+                    alert("Não foi possivel alterar o Produto");
                 }
             })
         }
 
         function inserir($categoria, $codigoBarra, $quantidadeProdutos, $valorUnitario) {
             $.ajax({
-                url: 'http://localhost:51242/Produto/inserir',
+                url: "http://localhost:51242/Produto/inserir",
                 method: "post",
                 data: {
                     categoria: $categoria,
@@ -64,7 +64,7 @@
                     valorUnitario: $valorUnitario
                 },
                 success: function (data) {
-                    $('modal-produto').modal('hide');
+                    $("modal-produto").modal("hide");
                     $tabelaProduto.ajax.reload();
                 },
                 error: function (err) {
@@ -74,12 +74,12 @@
         }
     });
 
-    $('.table').on('click', '.botao-apagar', function () {
-        $idApagar = $(this).data('id');
+    $(".table").on("click", ".botao-apagar", function () {
+        $idApagar = $(this).data("id");
 
         $ajax({
-            url: 'http://localhost:51242/Produto/apagar?=' + $idAlterar,
-            method: 'get',
+            url: "http://localhost:51242/Produto/apagar?=" + $idAlterar,
+            method: "get",
             success: function (data) {
                 $tabelaProduto.ajax.reload();
             },
@@ -89,18 +89,18 @@
         });
     });
 
-    $('.table').on('click', 'botao-editar', function () {
-        $idAlterar = $(this).data('id');
+    $(".table").on("click", "botao-editar", function () {
+        $idAlterar = $(this).data("id");
 
         $.ajax({
-            url: 'http://localhost:51242/Produto/obterpeloid?=' + idAlterar,
-            methd: 'get',
+            url: "http://localhost:51242/Produto/obterpeloid?=" + idAlterar,
+            methd: "get",
             success: function (data) {
-                $('#produto-campo-categoria').val(data.Categoria);
-                $('#produto-campo-codigo-barra').val(data.CodigoBarra);
-                $('#produto-campo-quantidade-produtos').val(data.QuantidadeProdutos);
-                $('#produto-campo-valor-unitatio').val(data.ValorUnitario);
-                $('#modal-produto').modal('show');
+                $("#produto-campo-categoria").val(data.Categoria);
+                $("#produto-campo-codigo-barra").val(data.CodigoBarra);
+                $("#produto-campo-quantidade-produtos").val(data.QuantidadeProdutos);
+                $("#produto-campo-valor-unitatio").val(data.ValorUnitario);
+                $("#modal-produto").modal("show");
             },
             error: function (err) {
                 alert("Não foi possivel carregar");
