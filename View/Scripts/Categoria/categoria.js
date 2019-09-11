486b033867bb5ca78b413d6650027dfe0b79bcd2
Felipe Franco Weber <felipeweber2002@hotmail.com>
2019-09-11 08:55:01 -0300

    Corrigido problemas de inserção de usuário e categoria

--- View/Scripts/Categoria/categoria.js
+++ View/Scripts/Categoria/categoria.js
@@ -25,47 +25,49 @@
         }
 
         if ($idAlterar == -1) {
-            inserir($nome);
+            inserir($nome.trim());
+
         } else {
-            alterar($nome);
+            alterar($nome.trim());
         }
-    });
 
-    function alterar($nome) {
-        $.ajax({
-            url: 'http://localhost:51242/Categoria/editar',
-            method: 'post',
-            data: {
-                id: $idAlterar,
-                nome: $nome
-            },
-            success: function (data) {
-                $('#modal-categoria').modal('hide');
-                $idAlterar = -1;
-                $tabelaCategoria.ajax.reload();
-            },
-            error: function (err) {
-                alert('Não foi possivel alterar');
-            }
-        });
-    }
+        function alterar($nome) {
+            $.ajax({
+                url: 'http://localhost:51242/Categoria/editar',
+                method: 'post',
+                data: {
+                    id: $idAlterar,
+                    nome: $nome
+                },
+                success: function (data) {
+                    $('#modal-categoria').modal('hide');
+                    $idAlterar = -1;
+                    $tabelaCategoria.ajax.reload();
+                },
+                error: function (err) {
+                    alert('Não foi possivel alterar');
+                }
+            });
+        }
 
-    function inserir($nome) {
-       $.ajax({
-            url: 'http://localhost:51242/Categoria/inserir',
-            method: 'post',
-            data: {
-                nome: $nome
-            },
-            success: function (data) {
-                $('#modal-categoria').modal('hide');
-                $tabelaCategoria.ajax.reload();
-            },
-            error: function (err) {
-                alert('Não foi possivel inserir');
-            }
-        });
-    }
+        function inserir($nome) {
+            $.ajax({
+                url: 'http://localhost:51242/Categoria/inserir',
+                method: 'post',
+                data: {
+                    nome: $nome
+                },
+                success: function (data) {
+                    $('#modal-categoria').modal('hide');
+                    $tabelaCategoria.ajax.reload();
+                    alert("Registro Inserido Com Sucesso")
+                },
+                error: function (err) {
+                    alert('Não foi possivel inserir');
+                }
+            })
+        }
+    })
 
     $('.table').on('click', '.botao-apagar', function () {
         $idApagar = $(this).data('id');
@@ -95,6 +97,6 @@
             error: function (err) {
                 alert('Não foi possivel carregar');
             }
-        });
+        })
     });
 });
