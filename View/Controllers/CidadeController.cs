3579bf6ae3148df6ae8a646693c0847709fbc1ca
Henrique Montibeller <iquemontibeller@gmail.com>
2019-09-12 09:12:51 -0300

    Arrmado alguns errors que estavam ocorrendo na hora de Cadastrar um vendedor

---
+++ View/Controllers/CidadeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Repository.Repositories;
+
+namespace View.Controllers
+{
+    public class CidadeController : Controller
+    {
+        [Route("")]
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- View/Scripts/Usuario/usuario.js
+++ View/Scripts/Usuario/usuario.js
@@ -21,16 +21,16 @@
         $nome = $("#usuario-campo-nome").val();
         $senha = $("#usuario-campo-senha").val();
 
-        if (($nome.trim() == "") || ($senha.trim() == "")) {
+        if (($nome.trim() == "") || ($senha == "")) {
             alert("Preencha corretamente os campos");
             return null;
         }
 
         if ($idAlterar == -1) {
-            inserir($nome.trim(), $senha.trim());
+            inserir($nome.trim(), $senha);
 
         } else {
-            alterar($nome.trim(), $senha.trim());
+            alterar($nome.trim(), $senha);
         }
 
         function alterar($nome, $senha) {
