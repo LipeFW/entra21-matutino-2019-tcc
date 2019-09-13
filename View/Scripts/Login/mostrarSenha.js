a630356d0e83213c992b646994934681c99535c2
Henrique Montibeller <iquemontibeller@gmail.com>
2019-09-13 09:59:43 -0300

    Feito mais alteracoes

---
+++ View/Scripts/Login/mostrarSenha.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#mostrarSenha').on('click', function () {
+        var campoSenha = $('#senha');
+        var campoSenhaType = campoSenha.attr('type');
+        if (campoSenhaType == 'password') {
+            campoSenha.attr('type', 'text');
+            $(this).val('Hide');
+        } else {
+            campoSenha.attr('type', 'password');
+            $(this).val('Show');
+        }
+    });
+});
