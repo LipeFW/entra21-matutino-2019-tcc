a40f2fb4cac414722beab355ac278d5f35e45bbc
Nathan t <nathantheiss@gmail.com>
2019-09-13 10:57:54 -0300

    Adiocionando Mais Mascaras

--- View/Scripts/Mascaras/Mask.js
+++ View/Scripts/Mascaras/Mask.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $("#cliente-campo-telefone").mask("(99) 99999-9999");
     $("#veiculo-campo-anofabricacao").mask("9999");
     $("#veiculo-campo-numerocaminhao").mask("999999");
-    $("#veiculo-campo-placa").mask("AAA-9999");
+    $("#veiculo-campo-placa").mask("SSS-9999");
     $("#perfil-campo-telefone").mask("(99) 99999-9999");
     $("#perfil-campo-cpf").mask("000.000.000-00");
     $("#perfil-campo-cep").mask("99999-999");
