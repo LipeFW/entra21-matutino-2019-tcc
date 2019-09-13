17d639ef7cec531f4e7c4720bf35196dfac7b408
nathantheiss12 <51447497+nathantheiss12@users.noreply.github.com>
2019-09-13 09:08:08 -0300

    Add files via upload

---
+++ View/Scripts/Mascaras/Mask.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+ 
+    $("#cliente-campo-cpf").mask("000.000.000-00");
+    $("#cliente-campo-cnpj").mask("99.999.999/9999-99");
+    $("#cliente-campo-cep").mask("99999-999");
+    $("#cliente-campo-telefone").mask("(99) 99999-9999");
+    $("#veiculo-campo-anofabricacao").mask("9999");
+    $("#veiculo-campo-numerocaminhao").mask("99 9999");
+    $("#veiculo-campo-placa").mask("AAA-9999");
+
+
+
+});
+
+       
