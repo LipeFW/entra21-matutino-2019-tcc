17d50c99426ea18dadad552c64f2e8003f1f99ae
Felipe F. Weber <felipeweber2002@hotmail.com>
2019-09-16 08:54:08 -0300

    Refeita opção de alteração de senha

---
+++ View/Scripts/Usuario/config.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $('#alterarSenha').on('click', function () {
+
+        var senha = $('#perfil-campo-senha').val();
+        var senhaConfirm = $('#perfil-campo-senha2').val();
+
+        if ((senha.trim() == "") || (senhaConfirm.trim() == "")) {
+            alert("A senha não pode estar vazia");
+        }
+
+        else if (senha == senhaConfirm) {
+
+
+            alert("Senha alterada com sucesso");
+        } else {
+            alert("As senhas não coincidem");
+        }
+    });
+});
