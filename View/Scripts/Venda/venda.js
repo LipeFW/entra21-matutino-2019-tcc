b6d8b49df6517c8b5f5952c2470e52db2e4fb0bb
Eduardo Hausmann <eduardohausmann1705@gmail.com>
2019-10-03 10:48:03 -0300

    Feitas algumas modificações

--- View/Scripts/Venda/venda.js
+++ View/Scripts/Venda/venda.js
@@ -17,115 +17,115 @@
         ]
     });
 
-//    $('#venda-botao-salvar').on('click', function () {
-//        $vendedor = $('#venda-campo-vendedor').val();
-//        $cliente = $('#venda-campo-cliente').val();
-//        $total = $('#venda-campo-total').val();
-//        $desconto = $('#venda-campo-desconto').val();
+    $('#venda-botao-salvar').on('click', function () {
+        $vendedor = $('#venda-campo-vendedor').val();
+        $cliente = $('#venda-campo-cliente').val();
+        $total = $('#venda-campo-total').val();
+        $desconto = $('#venda-campo-desconto').val();
 
-//        if ($idAlterar == -1) {
-//            inserir($vendedor, $cliente, $total, $desconto);
-//        } else {
-//            alterar($vendedor, $cliente, $total, $desconto);
-//        }
-//    });
+        if ($idAlterar == -1) {
+            inserir($vendedor, $cliente, $total, $desconto);
+        } else {
+            alterar($vendedor, $cliente, $total, $desconto);
+        }
+    });
 
-//    function alterar($vendedor, $cliente, $total, $desconto) {
-//        $.ajax({
-//            url: 'http://localhost:51242/Venda/update',
-//            method: 'post',
-//            data: {
-//                id: $idAlterar,
-//                idVendedor: $vendedor,
-//                idCliente: $cliente,
-//                total: $total,
-//                desconto: $desconto
-//            },
-//            success: function (data) {
-//                $("#modal-venda").modal("hide");
-//                $idAlterar = -1;
-//                $tabelaVenda.ajax.reload();
-//                alert("Registro alterado com sucesso");
+    function alterar($vendedor, $cliente, $total, $desconto) {
+        $.ajax({
+            url: 'http://localhost:51242/Venda/update',
+            method: 'post',
+            data: {
+                id: $idAlterar,
+                idVendedor: $vendedor,
+                idCliente: $cliente,
+                total: $total,
+                desconto: $desconto
+            },
+            success: function (data) {
+                $("#modal-venda").modal("hide");
+                $idAlterar = -1;
+                $tabelaVenda.ajax.reload();
+                alert("Registro alterado com sucesso");
 
-//            },
-//            error: function (err) {
-//                alert("Não foi possivel alterar");
-//            }
-//        })
-//    }
+            },
+            error: function (err) {
+                alert("Não foi possivel alterar");
+            }
+        })
+    }
 
-//    function inserir($vendedor, $cliente, $total, $desconto) {
-//        $.ajax({
-//            url: 'http://localhost:51242/Venda/inserir',
-//            method: 'post',
-//            data: {
-//                idVendedor: $vendedor,
-//                idCliente: $cliente,
-//                total: $total,
-//                desconto: $desconto
-//            },
-//            success: function (data) {
-//                $("#modal-venda").modal("hide");
-//                $tabelaVenda.ajax.reload();
-//                alert('Registro inserido com sucesso');
-//            },
-//            error: function (err) {
-//                alert("Não foi possivel inserir");
-//            }
-//        });
-//    }
+    function inserir($vendedor, $cliente, $total, $desconto) {
+        $.ajax({
+            url: 'http://localhost:51242/Venda/inserir',
+            method: 'post',
+            data: {
+                idVendedor: $vendedor,
+                idCliente: $cliente,
+                total: $total,
+                desconto: $desconto
+            },
+            success: function (data) {
+                $("#modal-venda").modal("hide");
+                $tabelaVenda.ajax.reload();
+                alert('Registro inserido com sucesso');
+            },
+            error: function (err) {
+                alert("Não foi possivel inserir");
+            }
+        });
+    }
 
-//$("#venda-tabela").on("click", ".botao-apagar", function () {
-//    $idApagar = $(this).data("id");
-//    bootbox.confirm({
-//        message: "Deseja realmente apagar o registro?",
-//        buttons: {
-//            confirm: {
-//                label: 'Sim',
-//                className: 'btn-success'
-//            },
-//            cancel: {
-//                label: 'Não',
-//                className: 'btn-danger'
-//            }
-//        },
-//        callback: function (result) {
-//            if (result)
-//                $.ajax({
-//                    url: "http://localhost:51242/Venda/Apagar?id=" + $idApagar,
-//                    method: "get",
-//                    success: function (data) {
-//                        $tabelaVenda.ajax.reload();
-//                        bootbox.alert("Registro apagado com sucesso");
-//
-//                    },
-//                    error: function (err) {
-//                        bootbox.alert('Não foi possível apagar');
-//                    }
-//                });
-//        }
-//    });
-//});
+$("#venda-tabela").on("click", ".botao-apagar", function () {
+    $idApagar = $(this).data("id");
+    bootbox.confirm({
+        message: "Deseja realmente apagar o registro?",
+        buttons: {
+            confirm: {
+                label: 'Sim',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'Não',
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            if (result)
+                $.ajax({
+                    url: "http://localhost:51242/Venda/Apagar?id=" + $idApagar,
+                    method: "get",
+                    success: function (data) {
+                        $tabelaVenda.ajax.reload();
+                        bootbox.alert("Registro apagado com sucesso");
 
-//    $('.table').on('click', '.botao-editar', function () {
-//        $idAlterar = $(this).data('id');
+                    },
+                    error: function (err) {
+                        bootbox.alert('Não foi possível apagar');
+                    }
+                });
+        }
+    });
+});
 
-//        $.ajax({
-//            url: 'http://localhost:51242/Venda/obterpeloid?id=' + $idAlterar,
-//            method: 'get',
+    $('.table').on('click', '.botao-editar', function () {
+        $idAlterar = $(this).data('id');
 
-//            success: function (data) {
-//                $('#venda-campo-vendedor').val(data.Vendedor);
-//                $('#venda-campo-cliente').val(data.Cliente);
-//                $('#venda-campo-total').val(data.Total);
-//                $('#venda-campo-desconto').val(data.Desconto);
+        $.ajax({
+            url: 'http://localhost:51242/Venda/obterpeloid?id=' + $idAlterar,
+            method: 'get',
 
-//                $('#modal-venda').modal('show');
+            success: function (data) {
+                $('#venda-campo-vendedor').val(data.Vendedor);
+                $('#venda-campo-cliente').val(data.Cliente);
+                $('#venda-campo-total').val(data.Total);
+                $('#venda-campo-desconto').val(data.Desconto);
 
-//            },
-//            error: function (err) {
-//                alert("Não foi possivel editar");
-//            }
-//        })
-//    });
+                $('#modal-venda').modal('show');
+
+            },
+            error: function (err) {
+                alert("Não foi possivel editar");
+            }
+        })
+    });
 //})
