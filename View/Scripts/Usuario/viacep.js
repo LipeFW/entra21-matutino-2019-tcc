48ed0b77467c7f4ca0cc5051a10929b06f4643e4
Illan Dickmann <illandickmann1@gmail.com>
2019-10-04 11:10:48 -0300

    Pequenas correções

--- View/Scripts/Usuario/viacep.js
+++ View/Scripts/Usuario/viacep.js
@@ -36,6 +36,7 @@
                 $("#config-campo-cidade").val($localidade);
                 $("#config-campo-logradouro").val($logradouro);
                 $('#config-campo-estado').val($unidadeFederativa).trigger('change');
+
                 //$("#config-campo-estado").val($unidadeFederativa);
                 $("#config-campo-numero").val("");
                 $("#config-campo-numero").focus();
